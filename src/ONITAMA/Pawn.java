6edc9225246522147261cde18f58561179eee107
inesh <inesh@DESKTOP-JL9QQ0S.education.lakanal.epf.fr>
2023-12-08 08:45:00 +0100

    ONITAMA

--- src/ONITAMA/Pawn.java
+++ src/ONITAMA/Pawn.java
@@ -9,6 +9,20 @@ package ONITAMA;
  * @author anaza
  */
 public class Pawn {
+    String nom; 
     String type;
+    int ligne; 
+    int colonne;
     
+
+    public Pawn(String type, int ligne, int colonne) {
+        this.ligne =  ligne;
+        this.colonne = colonne;
+        this.type = type;
+        
+    }
+    
+    public String VisualiserType(pawn){
+        return type;
+    }
 }
