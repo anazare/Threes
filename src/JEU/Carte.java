abccc69aed84d8c99093de45a8847d1a26fa73c0
ana_rita nazare_pinheiro <anarita.nazarepinheiro@epfedu.fr>
2023-12-13 12:39:30 +0100

    action listner carte

--- src/JEU/Carte.java
+++ src/JEU/Carte.java
@@ -19,8 +19,9 @@ import javax.swing.JButton;
  *
  * @author redou
  */
-public class Carte extends JButton() {
+public class Carte extends JButton {
     CarteConsole CarteAssociee;
+    
     int l;
     int L;
 
@@ -33,140 +34,137 @@ public class Carte extends JButton() {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        BufferedImage Image = null;
+        this.setText(CarteAssociee.toString());
+        ///BufferedImage Image = null;
         
-        switch (CarteAssociee.nom){
-            case "boar" : 
-            try {
-                Image = ImageIO.read(new File("boar.png"));
-            } catch (IOException ex) {
-                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            Image carte1 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
-            ImageIcon icon1 = new ImageIcon(carte1);
-            
-           //action listener + creation du jButton possible ? 
-            
-            case "cobra" : 
-            try {
-                Image = ImageIO.read(new File("cobra.png"));
-            } catch (IOException ex) {
-                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            Image carte2 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
-            ImageIcon icon2 = new ImageIcon(carte2);
-            
-            case "crab" : 
-                try {
-                Image = ImageIO.read(new File("crab.png"));
-            } catch (IOException ex) {
-                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            Image carte3 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
-            ImageIcon icon3 = new ImageIcon(carte3);
-            
-            case "crane":
-                try {
-                Image = ImageIO.read(new File("crane.png"));
-            } catch (IOException ex) {
-                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            Image carte4 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
-            ImageIcon icon4 = new ImageIcon(carte4);
-            
-            case "dragon" : 
-            try {
-                Image = ImageIO.read(new File("dragon.png"));
-            } catch (IOException ex) {
-                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            Image carte5 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
-            ImageIcon icon5 = new ImageIcon(carte5);
-            
-            case "eel":
-            try {
-                Image = ImageIO.read(new File("eel.png"));
-            } catch (IOException ex) {
-                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            Image carte6 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
-            ImageIcon icon6 = new ImageIcon(carte6);
-            
-            case "elephant":
-            try {
-                Image = ImageIO.read(new File("elephant.png"));
-            } catch (IOException ex) {
-                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            Image carte7 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
-            ImageIcon icon7 = new ImageIcon(carte7);
-            
-            case "frog":
-            try {
-                Image = ImageIO.read(new File("frog.png"));
-            } catch (IOException ex) {
-                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            Image carte8 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
-            ImageIcon icon8 = new ImageIcon(carte8);
-            
-            case "goose":
-                try {
-                Image = ImageIO.read(new File("goose.png"));
-            } catch (IOException ex) {
-                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            Image carte9 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
-            ImageIcon icon9 = new ImageIcon(carte9);
-            
-            case "horse":
-                try {
-                Image = ImageIO.read(new File("horse.png"));
-            } catch (IOException ex) {
-                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            Image carte10 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
-            ImageIcon icon10 = new ImageIcon(carte10);
-            
-            case "mantis": 
-                try {
-                Image = ImageIO.read(new File("mantis.png"));
-            } catch (IOException ex) {
-                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            Image carte11 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
-            ImageIcon icon11 = new ImageIcon(carte11);
-            
-            case "monkey":
-                try {
-                Image = ImageIO.read(new File("monkey.png"));
-            } catch (IOException ex) {
-                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            Image carte12 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
-            ImageIcon icon12 = new ImageIcon(carte12);
-            
-            case "ox":
-                try {
-                Image = ImageIO.read(new File("ox.png"));
-            } catch (IOException ex) {
-                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            Image carte13 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
-            ImageIcon icon13 = new ImageIcon(carte13);
-            
-            case "rabbit":
-            case "rooster":
-            case "tiger": 
-        }
-        try {
-            Image = ImageIO.read(new File("boar.png"));
-        } catch (IOException ex) {
-            Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        Image image1 = Image.getScaledInstance(40, 40, Image.SCALE_DEFAULT);
-        ImageIcon icon1 = new ImageIcon(image1);
-        
+//        switch (CarteAssociee.nom){
+//            case "boar" : 
+//            try {
+//                Image = ImageIO.read(new File("boar.png"));
+//            } catch (IOException ex) {
+//                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//            Image carte1 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
+//            ImageIcon icon1 = new ImageIcon(carte1);
+//            
+//           //action listener + creation du jButton possible ? 
+//            
+//            case "cobra" : 
+//            BufferedImage bufferedImage = ImageIO.read(new File("cobra.png"));
+//            Image carte2 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
+//            ImageIcon icon2 = new ImageIcon(carte2);
+//            
+//            case "crab" : 
+//                try {
+//                Image = ImageIO.read(new File("crab.png"));
+//            } catch (IOException ex) {
+//                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//            Image carte3 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
+//            ImageIcon icon3 = new ImageIcon(carte3);
+//            
+//            case "crane":
+//                try {
+//                Image = ImageIO.read(new File("crane.png"));
+//            } catch (IOException ex) {
+//                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//            Image carte4 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
+//            ImageIcon icon4 = new ImageIcon(carte4);
+//            
+//            case "dragon" : 
+//            try {
+//                Image = ImageIO.read(new File("dragon.png"));
+//            } catch (IOException ex) {
+//                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//            Image carte5 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
+//            ImageIcon icon5 = new ImageIcon(carte5);
+//            
+//            case "eel":
+//            try {
+//                Image = ImageIO.read(new File("eel.png"));
+//            } catch (IOException ex) {
+//                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//            Image carte6 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
+//            ImageIcon icon6 = new ImageIcon(carte6);
+//            
+//            case "elephant":
+//            try {
+//                Image = ImageIO.read(new File("elephant.png"));
+//            } catch (IOException ex) {
+//                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//            Image carte7 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
+//            ImageIcon icon7 = new ImageIcon(carte7);
+//            
+//            case "frog":
+//            try {
+//                Image = ImageIO.read(new File("frog.png"));
+//            } catch (IOException ex) {
+//                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//            Image carte8 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
+//            ImageIcon icon8 = new ImageIcon(carte8);
+//            
+//            case "goose":
+//                try {
+//                Image = ImageIO.read(new File("goose.png"));
+//            } catch (IOException ex) {
+//                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//            Image carte9 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
+//            ImageIcon icon9 = new ImageIcon(carte9);
+//            
+//            case "horse":
+//                try {
+//                Image = ImageIO.read(new File("horse.png"));
+//            } catch (IOException ex) {
+//                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//            Image carte10 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
+//            ImageIcon icon10 = new ImageIcon(carte10);
+//            
+//            case "mantis": 
+//                try {
+//                Image = ImageIO.read(new File("mantis.png"));
+//            } catch (IOException ex) {
+//                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//            Image carte11 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
+//            ImageIcon icon11 = new ImageIcon(carte11);
+//            
+//            case "monkey":
+//                try {
+//                Image = ImageIO.read(new File("monkey.png"));
+//            } catch (IOException ex) {
+//                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//            Image carte12 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
+//            ImageIcon icon12 = new ImageIcon(carte12);
+//            
+//            case "ox":
+//                try {
+//                Image = ImageIO.read(new File("ox.png"));
+//            } catch (IOException ex) {
+//                Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//            Image carte13 = Image.getScaledInstance(300, 175, Image.SCALE_DEFAULT);
+//            ImageIcon icon13 = new ImageIcon(carte13);
+//            
+//            case "rabbit":
+//            case "rooster":
+//            case "tiger": 
+//        }
+//        try {
+//            Image = ImageIO.read(new File("boar.png"));
+//        } catch (IOException ex) {
+//            Logger.getLogger(Carte.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        Image image1 = Image.getScaledInstance(40, 40, Image.SCALE_DEFAULT);
+//        ImageIcon icon1 = new ImageIcon(image1);
+//        
         
     }
     
