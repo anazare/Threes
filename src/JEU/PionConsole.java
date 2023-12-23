dc9e718ad31cd38cddf6600c46831d4dce47cb5a
anazare <145278184+anazare@users.noreply.github.com>
2023-12-23 23:31:30 +0100

    Update PionConsole.java

--- src/JEU/PionConsole.java
+++ src/JEU/PionConsole.java
@@ -15,6 +15,14 @@ public class PionConsole {
     int Y;
     String couleur;
 
+       /**
+     * Construit un pion avec en paramètres sa position X et Y (lignes/colonnes), 
+     * sa couleur et s'il s'agit d'un roi.
+     * @param X
+     * @param Y
+     * @param couleur
+     * @param roi
+     */
     public PionConsole(int X, int Y, String couleur,String roi) {
         this.X = X;
         this.Y = Y;
@@ -22,7 +30,9 @@ public class PionConsole {
         this.roi = roi;
     }
     
-    
+    /**
+     * Supprime le pion en changeant sa couleur vers "white".
+     */
    public void SuppPion(){
        couleur = "white";
     }
@@ -35,18 +45,29 @@ public class PionConsole {
         gris = false;
     }
 
+    /**
+     * Ajoute un pion en fonction de sa position, sa couleur et de son type 
+     */
     public void addPion(int X1, int Y1, String couleur1,String roi1){
         X=X1;
         Y=Y1;
         couleur=couleur1;
         roi = roi1;
     }
-    
+
+  /**
+     * Déplace un pion en fonction de sa couleur et de son type 
+     */
     public void deplace(String couleur1,String roi1){
         couleur = couleur1;
         roi = roi1;
     }
 
+  /**
+     * Ce toString retourne en fonction de la couleur et du type du pions RR (roi rouge), PR (pion rouge),
+     * RB (roi bleu), PB(pion bleu).
+     * @return
+     */
     @Override
     public String toString() {
           if (couleur == "white"){
