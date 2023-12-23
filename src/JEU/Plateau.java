a2f3e3265eb2a1bb636cdc6c031ff92db9882bba
redouane ndiaye <oumar.ndiaye@epfedu.fr>
2023-12-23 21:48:06 +0100

    ANNA c a toi

--- src/JEU/Plateau.java
+++ src/JEU/Plateau.java
@@ -17,22 +17,22 @@ public class Plateau {
     Joueur j1 ;
     Joueur j2 ;
     ArrayList<CarteConsole> Main;
-    Coord nulle = new Coord(20,20),boar1 =new Coord(-1,0),boar2=new Coord(0,1),boar3 = new Coord(0,-1);
-    Coord cobra1 = new Coord(0,-1),cobra2 =new Coord(-1,1),cobra3=new Coord(1,1);
-    Coord crab1 = new Coord(-2,0),crab2 =new Coord(2,0),crab3=new Coord(0,-1);
-    Coord crane1 = new Coord(-1,0),crane2 =new Coord(1,1),crane3=new Coord(1,-1);
-    Coord dragon1 = new Coord(1,-1),dragon2 =new Coord(1,1),dragon3=new Coord(-1,2),dragon4 = new Coord(-1,2);
-    Coord eel1 = new Coord(1,-1),eel2 =new Coord(0,1),eel3=new Coord(-1,-1);
-    Coord elephant1 = new Coord(0,-1),elephant2 =new Coord(0,1),elephant3=new Coord(-1,-1),elephant4 = new Coord(-1,1);
-    Coord frog1 = new Coord(0,-2),frog2 =new Coord(-1,-1),frog3=new Coord(1,1);
-    Coord goose1 = new Coord(0,1),goose2 =new Coord(0,-1),goose3=new Coord(-1,-1),goose4 = new Coord(1,1);
-    Coord horse1 = new Coord(0,-1),horse2 =new Coord(1,0),horse3=new Coord(-1,0);
-    Coord mantis1 = new Coord(1,0),mantis2 =new Coord(-1,-1),mantis3=new Coord(-1,1);
+    Coord nulle = new Coord(20,20),boar1 =new Coord(-1,0),boar2=new Coord(1,0),boar3 = new Coord(0,1);
+    Coord cobra1 = new Coord(-1,0),cobra2 =new Coord(1,-1),cobra3=new Coord(1,1);
+    Coord crab1 = new Coord(-2,0),crab2 =new Coord(2,0),crab3=new Coord(0,1);
+    Coord crane1 = new Coord(0,1),crane2 =new Coord(-1,-1),crane3=new Coord(1,-1);
+    Coord dragon1 = new Coord(-1,-1),dragon2 =new Coord(1,-1),dragon3=new Coord(-2,1),dragon4 = new Coord(2,1);
+    Coord eel1 = new Coord(-1,-1),eel2 =new Coord(1,0),eel3=new Coord(-1,1);
+    Coord elephant1 = new Coord(-1,0),elephant2 =new Coord(1,0),elephant3=new Coord(-1,1),elephant4 = new Coord(1,1);
+    Coord frog1 = new Coord(-2,0),frog2 =new Coord(-1,1),frog3=new Coord(1,-1);
+    Coord goose1 = new Coord(1,0),goose2 =new Coord(-1,0),goose3=new Coord(-1,1),goose4 = new Coord(1,-1);
+    Coord horse1 = new Coord(0,-1),horse2 =new Coord(-1,0),horse3=new Coord(0,1);
+    Coord mantis1 = new Coord(0,-1),mantis2 =new Coord(-1,1),mantis3=new Coord(1,1);
     Coord monkey1 = new Coord(1,-1),monkey2 =new Coord(1,1),monkey3=new Coord(-1,1),monkey4 = new Coord(-1,-1);
-    Coord ox1 = new Coord(0,1),ox2 =new Coord(-1,0),ox3=new Coord(1,0);
-    Coord rabbit1 = new Coord(1,-1),rabbit2 =new Coord(0,2),rabbit3=new Coord(-1,1);
-    Coord rooster1 = new Coord(1,-1),rooster2 =new Coord(-1,1),rooster3=new Coord(0,1),rooster4 = new Coord(0,-1);
-    Coord tiger1 = new Coord(-2,0),tiger2 =new Coord(1,0); 
+    Coord ox1 = new Coord(0,1),ox2 =new Coord(0,-1),ox3=new Coord(1,0);
+    Coord rabbit1 = new Coord(-1,-1),rabbit2 =new Coord(2,0),rabbit3=new Coord(1,1);
+    Coord rooster1 = new Coord(-1,-1),rooster2 =new Coord(-1,0),rooster3=new Coord(1,0),rooster4 = new Coord(1,1);
+    Coord tiger1 = new Coord(0,2),tiger2 =new Coord(0,-1);
     CarteConsole boar = new CarteConsole("boar",boar1,boar2,boar3,nulle);
     CarteConsole cobra = new CarteConsole("cobra",cobra1,cobra2,cobra3,nulle);
     CarteConsole crab = new CarteConsole("crab",crab1,crab2,crab3,nulle);
@@ -149,7 +149,6 @@ public class Plateau {
     }
     
     public void initialiserPion(){
-        grille.grille[2][2].pion.addPion(2,2,"blue","non");
         for (int j=0; j<taille;j++){
             if(j!=2){
                 grille.grille[j][0].pion.addPion(j,0,"blue","non");
